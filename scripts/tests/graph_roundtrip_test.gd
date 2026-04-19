1302e06aed51c28eca476d3ab0013a5268d31eec
UmbrellaSampler <uwe.ehmann@googlemail.com>
2026-04-19 19:32:19 +0200

    refac: use global class names instead of script preload aliases
    Issue: #21

--- scripts/tests/graph_roundtrip_test.gd
+++ scripts/tests/graph_roundtrip_test.gd
@@ -1,9 +1,5 @@
 extends SceneTree
 
-const TransportGraphScript = preload("res://scripts/transport_graph/transport_graph.gd")
-const TransportNodeScript = preload("res://scripts/transport_graph/transport_node.gd")
-const TransportLinkScript = preload("res://scripts/transport_graph/transport_link.gd")
-
 const FIRST_SAVE_PATH := "user://graph_roundtrip_a.json"
 const SECOND_SAVE_PATH := "user://graph_roundtrip_b.json"
 
@@ -11,7 +7,7 @@ func _init() -> void:
 	var graph = _build_sample_graph()
 	graph.save_to_json_file(FIRST_SAVE_PATH)
 
-	var loaded = TransportGraphScript.load_from_json_file(FIRST_SAVE_PATH)
+	var loaded = TransportGraph.load_from_json_file(FIRST_SAVE_PATH)
 	loaded.save_to_json_file(SECOND_SAVE_PATH)
 
 	var first_data = _read_json_dictionary(FIRST_SAVE_PATH)
@@ -26,16 +22,16 @@ func _init() -> void:
 	quit(0)
 
 
-func _build_sample_graph():
-	var graph = TransportGraphScript.new()
+func _build_sample_graph() -> TransportGraph:
+	var graph := TransportGraph.new()
 	graph.id = "test-graph"
 	graph.name = "Roundtrip Test"
 
-	graph.add_node(TransportNodeScript.new("n-a", Vector3(1, 2, 3), "A"))
-	graph.add_node(TransportNodeScript.new("n-b", Vector3(4, 5, 6), "B"))
-	graph.add_node(TransportNodeScript.new("n-c", Vector3(-1, 0, 9.25), "C"))
+	graph.add_node(TransportNode.new("n-a", Vector3(1, 2, 3), "A"))
+	graph.add_node(TransportNode.new("n-b", Vector3(4, 5, 6), "B"))
+	graph.add_node(TransportNode.new("n-c", Vector3(-1, 0, 9.25), "C"))
 
-	graph.add_link(TransportLinkScript.new(
+	graph.add_link(TransportLink.new(
 		"l-ab",
 		"n-a",
 		"n-b",
@@ -43,7 +39,7 @@ func _build_sample_graph():
 		[Vector3(1.5, 2.5, 3.5), Vector3(2.5, 3.5, 4.5)],
 		"A to B"
 	))
-	graph.add_link(TransportLinkScript.new(
+	graph.add_link(TransportLink.new(
 		"l-bc",
 		"n-b",
 		"n-c",
